0fe0eab8990870f803f959fdc1f11d6b6898de37
nextacademy <cheekokfoong@gmail.com>
2016-11-02 18:08:35 +0800

    done with omniauth-facebook

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,10 @@ class UsersController < ApplicationController
   def update
   end
   
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :encrypted_password, :provider,:uid)
+  end
+
 end
