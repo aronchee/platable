c07e913034f986d04e40becedc6011a94c85f730
nextacademy <cheekokfoong@gmail.com>
2016-11-02 15:40:05 +0800

    done with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +54,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get 'users/edit'
+  get 'users/update'
+
+  devise_for :users
+  get 'welcome/index'
 end
