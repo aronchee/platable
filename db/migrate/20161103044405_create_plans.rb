bf9773caa1253b14057482cec0fa066ae82f86a4
bri-x <leong.brigitte@gmail.com>
2016-11-03 18:49:50 +0800

    mend

--- db/migrate/20161103044405_create_plans.rb
+++ db/migrate/20161103044405_create_plans.rb
@@ -1,8 +1,8 @@
 class CreatePlans < ActiveRecord::Migration
   def change
     create_table :plans do |t|
-      t.integer :user_id
-      t.integer :recipe_id
+      t.integer :user_id, index: true
+      t.integer :recipe_id, index: true
       t.date :date
 
       t.timestamps null: false
