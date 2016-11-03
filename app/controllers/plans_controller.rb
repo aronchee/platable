ad673165379b87b8ef3b108f72293f121c147885
Tan Jit SIang <joetjsjoe@hotmail.com>
2016-11-03 18:31:14 +0800

    just need to added seach funtionality in the new form

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,11 +4,10 @@ class PlansController < ApplicationController
     @plans = Plan.where(user_id: params[:user_id])
     @user = current_user
     @date = params[:date]
-
   end
 
   def create
-    # byebug
+
     @plan = Plan.new(plan_params)
     @plan[:user_id] = params[:user_id]
     @plan.save
@@ -22,27 +21,12 @@ class PlansController < ApplicationController
     @plans = Plan.where(user_id: params[:user_id])
 
     @day1 = @plans.where(date: Date.today.strftime('%Y-%m-%d'))
-    @day2= @plans.where(date: 1.days.since.strftime('%Y-%m-%d'))
-    @day3= @plans.where(date: 2.days.since.strftime('%Y-%m-%d'))
-    @day4= @plans.where(date: 3.days.since.strftime('%Y-%m-%d'))
-    @day5= @plans.where(date: 4.days.since.strftime('%Y-%m-%d'))
-    @day6= @plans.where(date: 5.days.since.strftime('%Y-%m-%d'))
-    @day7= @plans.where(date: 6.days.since.strftime('%Y-%m-%d'))
-
-
-
-
 
   end
 
   def show
   end
 
-  def edit
-  end
-
-  def update
-  end
 
   def destroy
 
