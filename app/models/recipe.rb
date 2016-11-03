61b055d05d0948bebdbdc48e3041c015a03cf353
bri-x <leong.brigitte@gmail.com>
2016-11-03 18:43:10 +0800

    added filterable module to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,14 @@ class Recipe < ActiveRecord::Base
 	has_many :ingredients, through: :ingredient_amounts
 
 	validates :name, uniqueness: true
+
+	include Filterable
+
+	scope :location, ->(q) { includes(:city).where('cities.name ILIKE :query OR cities.country ILIKE :query', query: q).references(:cities) }
+	scope :price, -> (ranges) { where(price: ranges) }
+	scope :date, ->(dates) { where.not("'#{dates[:start]}' > ANY (unavailable_dates) AND '#{dates[:end]}' <= ANY (unavailable_dates)").where("min_stay <= ?", (Date.parse(date[:end]) - Date.parse(date[:start])).to_i) }
+	scope :capacity, ->(q) { where(capacity: q)}
+	scope :property_type, ->(types) { where(property_type: types) }
+	scope :room_type, ->(types) { where(room_type: types) }
+
 end
