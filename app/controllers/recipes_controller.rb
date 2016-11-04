6593877bd0d05fa06995ac5217bce75709db2656
bri-x <leong.brigitte@gmail.com>
2016-11-04 16:51:52 +0800

    added search function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,24 +9,28 @@ class RecipesController < ApplicationController
   end
 
   def search
-  	params[:date] ||= []
-  	@results = Listing.filter(search_filters)
-    @similar = Listing.filter(similar_filters) unless @results.count > 5
+  	params[:ingredient] ||= []
+    params[:filter] ||= {}
+    @all_ingredients = Ingredient.pluck(:name).sort
+    @recipes = Recipe.ingredient(params[:ingredient].reject(&:empty?)).filter(search_filters).simple
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
   def search_filters
-  	params.permit(:location, :capacity, :date => {}, :price => [], :room_type => [], :property_type => []).tap do |permitted|
-			permitted[:location] = params[:location]
-  		(permitted[:location].length).downto(0) { |i| permitted[:location] = permitted[:location].insert(i, "%")}
-  		permitted[:price] = params_to_range(params[:price]) if params[:price].present?
-  	end
-  end
-
-  def similar_filters
-    search_filters.tap do |permitted|
-			permitted[:price] = Range.new(permitted[:price].first.first.to_i/2, permitted[:price].last.last.to_i*1.25) if params[:price].present?
-  		permitted[:capacity] = Range.new(params[:capacity].to_i-1, params[:capacity].to_i+2) if params[:capacity].present?
+  	params.permit(:dairy, :eggs, :peanuts, :nuts, :soy, :gluten, :seafood, :vegetarian, :spicy, :cuisine, :category, :main, :time).tap do |permitted|
+      strings = []
+      params[:filter].each do |k, v| 
+        strings << k
+        strings << v
+        strings << "AND"
+      end
+      strings.pop
+      string = strings.join(" ")
+      permitted[:nutrition] = string
   	end
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,17 +1,33 @@
 class Recipe < ActiveRecord::Base
 	has_one :nutrition_estimate
-	has_many :ingredient_amounts
+	has_many :ingredient_amounts, dependent: :destroy
 	has_many :ingredients, through: :ingredient_amounts
 
 	validates :name, uniqueness: true
 
 	include Filterable
 
-	scope :location, ->(q) { includes(:city).where('cities.name ILIKE :query OR cities.country ILIKE :query', query: q).references(:cities) }
-	scope :price, -> (ranges) { where(price: ranges) }
-	scope :date, ->(dates) { where.not("'#{dates[:start]}' > ANY (unavailable_dates) AND '#{dates[:end]}' <= ANY (unavailable_dates)").where("min_stay <= ?", (Date.parse(date[:end]) - Date.parse(date[:start])).to_i) }
-	scope :capacity, ->(q) { where(capacity: q)}
-	scope :property_type, ->(types) { where(property_type: types) }
-	scope :room_type, ->(types) { where(room_type: types) }
+	scope :ingredient, -> (array) { joins(:ingredients).where(ingredients: {name: array}).group("recipes.id").order(count: :desc) }
+	scope :simple, -> { order("cardinality(ingredient_text)") }
+	scope :cuisine, -> (type) { where("categories @> ?", "{#{type}}") }
+	scope :category, -> (type) { where("categories @> ?", "{#{type}}") }
+	scope :main, -> (type) { where("categories @> ?", "{#{type}}") }
+	scope :time, -> (range) { where(time_in_min: range).order(:time_in_min) }
+	scope :nutrition, -> (array) { joins(:nutrition_estimate).where("#{array}") }
+	# exclusions
+	scope :dairy, -> (bool) { where.not(ingredients: {name: ["milk", "cheese", "cream", "custard", "yogurt", "pudding"] }) }
+	scope :eggs, -> (bool) { where.not(ingredients: {name: "egg"}) }
+	scope :peanuts, -> (bool) { where.not(ingredients: {name: "peanut"}) }
+	scope :nuts, -> (bool) { where.not(ingredients: {name: "nut"}) }
+	scope :soy, -> (bool) { where.not(ingredients: {name: "soy"}) }
+	scope :gluten, -> (bool) { where.not(ingredients: {name: ["oats", "flour"]}) }
+	scope :seafood, -> (bool) { where.not("categories @> ?", "{Seafood}") }
+	scope :vegetarian, -> (bool) { where.not("categories @> '{Meat and Poultry}' OR categories @> '{Seafood}'") }
+	scope :spicy, -> (bool) { where.not("categories @> '{Spicy}'").where.not("ingredients.name LIKE '%chili%'") }
 
+	EXCLUSION = ["dairy", "eggs", "peanuts", "nuts", "soy", "gluten", "seafood", "vegetarian", "spicy"]
+	PREP_TIME = {"< 30 min" => "0-30", "30 min - 1 hr" => "30-60", "1 - 2 hr" => "60-120", "2 - 3 hr" => "120-180", "> 3 hr" => "180-1000"}
+	CUISINE = ["Latin American", "American", "European", "Eastern European", "Asian"]
+	CATEGORY = ["Dips and Spreads", "Appetizers", "Snacks", "Main Dish", "Side Dish", "Desserts", "Everyday Cooking", "Kids", "Healthy", "Quick and Easy", "Cute and Fun"]
+	MAIN_INGREDIENT = ["Meat and Poultry", "Seafood", "Vegetable", "Fruit"]
 end
