cfc5940c7acfde956a95375d899b2090c0bd092f
bri-x <leong.brigitte@gmail.com>
2016-11-05 15:44:27 +0800

    edited design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def current_path
+		array = request.fullpath.split('/')
+		if array.size > 3
+			return array[3]
+		elsif array.size > 1
+			return array[1]
+		else
+			return ""
+		end
+	end
 end
